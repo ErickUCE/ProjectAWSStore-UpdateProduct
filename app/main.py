ca2030ce64cecf5e9790043e7a277241908b2cfc
erickuce1509 <erickuce1509@gmail.com>
2025-02-12 01:34:06 -0500

    feat:

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from app.routes import router
+from fastapi.middleware.cors import CORSMiddleware  # ✅ Importar CORSMiddleware
+
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Permitir solicitudes desde el frontend
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos los métodos (GET, POST, DELETE, etc.)
+    allow_headers=["*"],  # Permitir todos los encabezados
+)
 
 # ✅ Incluir las rutas
 app.include_router(router)
